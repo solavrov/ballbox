33e8bf9d54d24188419f49a884c36b1a9daf9155
solavrov <2slavrov@gmail.com>
2018-02-19 11:59:13 +0300

    vector is introduced

--- box.py
+++ box.py
@@ -1,9 +1,8 @@
 from kivy.uix.widget import Widget
 from kivy.graphics import Ellipse, Color
 from kivy.core.window import Window
-#from kivy.vector import Vector
+from kivy.vector import Vector
 from random import random as random
-from math import sqrt
 
 
 class Ball:
@@ -11,14 +10,13 @@ class Ball:
     def __init__(self):
         self.color = random()
         self.radius = 25
-        self.x = (Window.size[0] - 2 * self.radius) * random() + self.radius
-        self.y = (Window.size[1] - 2 * self.radius) * random() + self.radius
-        self.vx = 200 * (random() - 0.5)
-        self.vy = 200 * (random() - 0.5)
+        self.p = Vector((Window.size[0] - 2 * self.radius) * random() + self.radius,
+                        (Window.size[1] - 2 * self.radius) * random() + self.radius)
+        self.v = Vector(200 * (random() - 0.5), 200 * (random() - 0.5))
         self.image = None
 
     def get_pos(self):
-        return self.x - self.radius, self.y - self.radius
+        return self.p.x - self.radius, self.p.y - self.radius
 
     def draw(self, canvas):
         with canvas:
@@ -27,21 +25,21 @@ class Ball:
                                  size=(2 * self.radius, 2 * self.radius))
 
     def bounce_box(self):
-        if self.x < self.radius:
-            self.vx = abs(self.vx)
-            self.x = self.radius
-        if self.x > Window.size[0] - self.radius:
-            self.vx = - abs(self.vx)
-            self.x = Window.size[0] - self.radius
-        if self.y < self.radius:
-            self.vy = abs(self.vy)
-            self.y = self.radius
-        if self.y > Window.size[1] - self.radius:
-            self.vy = - abs(self.vy)
-            self.y = Window.size[1] - self.radius
+        if self.p.x < self.radius:
+            self.v.x = abs(self.v.x)
+            self.p.x = self.radius
+        if self.p.x > Window.size[0] - self.radius:
+            self.v.x = - abs(self.v.x)
+            self.p.x = Window.size[0] - self.radius
+        if self.p.y < self.radius:
+            self.v.y = abs(self.v.y)
+            self.p.y = self.radius
+        if self.p.y > Window.size[1] - self.radius:
+            self.v.y = - abs(self.v.y)
+            self.p.y = Window.size[1] - self.radius
 
-    def get_dist(self, ball):
-        return sqrt((self.x - ball.x)**2 + (self.y - ball.y)**2)
+    # def get_dist(self, ball):
+    #     return sqrt((self.x - ball.x)**2 + (self.y - ball.y)**2)
 
     # def bounce_balls(self, balls):
     #     for b in balls:
@@ -50,8 +48,7 @@ class Ball:
 
     def move(self, dt):
         self.bounce_box()
-        self.x += self.vx * dt
-        self.y += self.vy * dt
+        self.p += self.v * dt
         self.image.pos = self.get_pos()
 
 
