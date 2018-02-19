d6881094ee20b8d1c362d0f272ceff478ae498c4
solavrov <2slavrov@gmail.com>
2018-02-19 13:16:42 +0300

    bounce_ball()

--- box.py
+++ box.py
@@ -9,45 +9,45 @@ class Ball:
 
     def __init__(self):
         self.color = random()
-        self.radius = 25
-        self.p = Vector((Window.size[0] - 2 * self.radius) * random() + self.radius,
-                        (Window.size[1] - 2 * self.radius) * random() + self.radius)
+        self.r = 25
+        self.p = Vector((Window.size[0] - 2 * self.r) * random() + self.r,
+                        (Window.size[1] - 2 * self.r) * random() + self.r)
         self.v = Vector(200 * (random() - 0.5), 200 * (random() - 0.5))
         self.image = None
 
     def get_pos(self):
-        return self.p.x - self.radius, self.p.y - self.radius
+        return self.p.x - self.r, self.p.y - self.r
 
     def draw(self, canvas):
         with canvas:
             Color(self.color, 1, 1, mode="hsv")
             self.image = Ellipse(pos=self.get_pos(),
-                                 size=(2 * self.radius, 2 * self.radius))
+                                 size=(2 * self.r, 2 * self.r))
 
     def bounce_box(self):
-        if self.p.x < self.radius:
+        if self.p.x < self.r:
             self.v.x = abs(self.v.x)
-            self.p.x = self.radius
-        if self.p.x > Window.size[0] - self.radius:
+            self.p.x = self.r
+        if self.p.x > Window.size[0] - self.r:
             self.v.x = - abs(self.v.x)
-            self.p.x = Window.size[0] - self.radius
-        if self.p.y < self.radius:
+            self.p.x = Window.size[0] - self.r
+        if self.p.y < self.r:
             self.v.y = abs(self.v.y)
-            self.p.y = self.radius
-        if self.p.y > Window.size[1] - self.radius:
+            self.p.y = self.r
+        if self.p.y > Window.size[1] - self.r:
             self.v.y = - abs(self.v.y)
-            self.p.y = Window.size[1] - self.radius
+            self.p.y = Window.size[1] - self.r
 
-    # def get_dist(self, ball):
-    #     return sqrt((self.x - ball.x)**2 + (self.y - ball.y)**2)
-
-    # def bounce_balls(self, balls):
-    #     for b in balls:
-    #         if self.get_dist(b) < self.radius + b.radius:
-    #             self.
+    def bounce_ball(self, ball): #clinging is still possible
+        d = self.p.distance(ball.p)
+        if d < self.r + ball.r:
+            e = (ball.p - self.p).normalize()
+            self.v -= 2 * e.dot(self.v) * e
+            ball.v -= 2 * e.dot(ball.v) * e
+            self.p -= (self.r + ball.r - d) / 2 * e
+            ball.p += (self.r + ball.r - d) / 2 * e
 
     def move(self, dt):
-        self.bounce_box()
         self.p += self.v * dt
         self.image.pos = self.get_pos()
 
@@ -65,5 +65,11 @@ class Box(Widget):
         return self
 
     def move(self, dt):
+        n = self.balls.__len__()
+        for i in range(n):
+            self.balls[i].bounce_box()
+            for j in range(i + 1, n):
+                self.balls[i].bounce_ball(self.balls[j])
         for b in self.balls:
             b.move(dt)
+
--- main.py
+++ main.py
@@ -10,8 +10,9 @@ class BallBoxApp(App):
         print(Window.size)
 
         box = Box()
-        box.add_ball().add_ball().add_ball().add_ball().add_ball()
-        box.add_ball().add_ball().add_ball().add_ball().add_ball()
+        box.add_ball().add_ball().add_ball().add_ball()
+        box.add_ball().add_ball().add_ball().add_ball()
+        box.add_ball().add_ball().add_ball().add_ball()
         Clock.schedule_interval(box.move, 1.0 / 60.0)
 
         return box
